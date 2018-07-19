d8e2df76d900a35d4a81a0e1f45559a4a9e498bf
Abel Trotter and Kevin Ahn <kevin@>
2018-07-19 10:46:07 -0700

    Refactor Books controller for many to many author relationship

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -24,20 +24,24 @@ namespace Library.Controllers
         [HttpGet("/books/new")]
         public ActionResult Create()
         {
-            ViewBag.AuthorId = db.Authors.ToList()
+            ViewBag.AuthorIds = db.Authors.ToList()
                 .Select(author => new SelectListItem {
-                    Value = author.AuthorId.ToString(), Text = author.AuthorFirstName + " " + author.AuthorLastName
+                    Value = author.AuthorId.ToString(),
+                    Text = author.AuthorFirstName + " " + author.AuthorLastName
                 })
                 .ToList();
             return View();
         }
 
         [HttpPost("/books/new")]
-        public ActionResult Create(Book book, int AuthorId)
+        public ActionResult Create(Book book, List<int> AuthorIds)
         {
             db.Books.Add(book);
-            BookAuthor newBookAuthor = new BookAuthor(book.BookId, AuthorId);
-            db.BookAuthors.Add(newBookAuthor);
+            foreach (int authorId in AuthorIds)
+            {
+                BookAuthor newBookAuthor = new BookAuthor(book.BookId, authorId);
+                db.BookAuthors.Add(newBookAuthor);
+            }
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -50,7 +54,7 @@ namespace Library.Controllers
             List<Author> authorList = new List<Author>();
             foreach (var author in entryList)
             {
-                int authorId = int.Parse(author.AuthorId.ToString());
+                int authorId = author.AuthorId;
                 authorList.Add(db.Authors.FirstOrDefault(record => record.AuthorId == authorId));
             }
             ViewBag.AuthorList = authorList;
@@ -61,18 +65,39 @@ namespace Library.Controllers
         public ActionResult Edit(int id)
         {
             Book book = db.Books.FirstOrDefault(books => books.BookId == id);
-            ViewBag.AuthorId = db.Authors.ToList()
+            ViewBag.AuthorIds = db.Authors.ToList()
                 .Select(author => new SelectListItem {
-                    Value = author.AuthorId.ToString(), Text = author.AuthorFirstName + " " + author.AuthorLastName
+                    Value = author.AuthorId.ToString(),
+                    Text = author.AuthorFirstName + " " + author.AuthorLastName
                 })
                 .ToList();
             return View(book);
         }
 
         [HttpPost("books/{id}/edit")]
-        public ActionResult Edit(Book book)
+        public ActionResult Edit(Book book, List<int> AuthorIds)
         {
             db.Entry(book).State = EntityState.Modified;
+            var bookMatchesInJoinTable = db.BookAuthors.Where(entry => entry.BookId == book.BookId).ToList();
+            // remove all BookAuthor entries for specified book.
+            foreach (var author in bookMatchesInJoinTable)
+            {
+                int authorId = author.AuthorId;
+                var joinEntry = db.BookAuthors
+                                  .Where(entry => entry.AuthorId == authorId)
+                                  .Where(entry => entry.BookId == book.BookId);
+                foreach (var entry in joinEntry)
+                {
+                    db.BookAuthors.Remove(entry);
+                }
+            }
+            // Add BookAuthor entries based on bookMatches.
+            foreach (var id in AuthorIds)
+            {
+                Author author = db.Authors.FirstOrDefault(_ => _.AuthorId == id);
+                BookAuthor newBookAuthor = new BookAuthor(book.BookId, author.AuthorId);
+                db.BookAuthors.Add(newBookAuthor);
+            }
             db.SaveChanges();
             return RedirectToAction("Index");
         }
