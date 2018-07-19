ba15c3a041a525171504dd67ba22b0039220aa1f
Abel Trotter <a_trotter0@hotmail.com>
2018-07-18 22:23:39 -0700

    Get create and details views working for Book controller

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -24,39 +24,53 @@ namespace Library.Controllers
         [HttpGet("/books/new")]
         public ActionResult Create()
         {
+            ViewBag.AuthorId = db.Authors.ToList()
+                .Select(author => new SelectListItem {
+                    Value = author.AuthorId.ToString(), Text = author.AuthorFirstName + " " + author.AuthorLastName
+                })
+                .ToList();
             return View();
         }
 
         [HttpPost("/books/new")]
-        public ActionResult Create(Book book)
+        public ActionResult Create(Book book, int AuthorId)
         {
             db.Books.Add(book);
+            BookAuthor newBookAuthor = new BookAuthor(book.BookId, AuthorId);
+            db.BookAuthors.Add(newBookAuthor);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
 
         [HttpGet("/books/{id}")]
         public ActionResult Details(int id)
         {
             Book book = db.Books.FirstOrDefault(books => books.BookId == id);
+            var entryList = db.BookAuthors.Where(entry => entry.BookId == id).ToList();
+            List<Author> authorList = new List<Author>();
+            foreach (var author in entryList)
+            {
+                int authorId = int.Parse(author.AuthorId.ToString());
+                authorList.Add(db.Authors.FirstOrDefault(record => record.AuthorId == authorId));
+            }
+            ViewBag.AuthorList = authorList;
             return View(book);
         }
 
         [HttpGet("books/{id}/edit")]
         public ActionResult Edit(int id)
         {
-            Book thisBook = db.Books.FirstOrDefault(books => books.BookId == id);
+            Book book = db.Books.FirstOrDefault(books => books.BookId == id);
             ViewBag.AuthorId = db.Authors.ToList()
                 .Select(author => new SelectListItem {
                     Value = author.AuthorId.ToString(), Text = author.AuthorFirstName + " " + author.AuthorLastName
                 })
                 .ToList();
-            return View(thisBook);
+            return View(book);
         }
 
         [HttpPost("books/{id}/edit")]
-        public ActionResult Edit(int id, Book book)
+        public ActionResult Edit(Book book)
         {
             db.Entry(book).State = EntityState.Modified;
             db.SaveChanges();
@@ -67,7 +81,9 @@ namespace Library.Controllers
         public ActionResult Delete(int id)
         {
             Book book = db.Books.FirstOrDefault(books => books.BookId == id);
+            BookAuthor joinEntry = db.BookAuthors.FirstOrDefault(entry => entry.BookId == id);
             db.Books.Remove(book);
+            db.BookAuthors.Remove(joinEntry);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
